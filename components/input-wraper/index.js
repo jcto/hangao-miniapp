4a582c3d402a0a717639c88bddc78de1d0de1ee8
cao.huanan <cao.huanan@opg.cn>
2020-05-12 12:42:42 +0800

    login

--- components/input-wraper/index.js
+++ components/input-wraper/index.js
@@ -1,3 +1,31 @@
 Component({
-    
+  properties: {
+    label: {
+      // 属性名
+      type: String,
+      value: "账号:",
+    },
+    required: {
+      type: Boolean,
+      value: true,
+    },
+    clearable: {
+      type: Boolean,
+      value: true,
+    },
+    placeholder:{
+      type:String,
+      value:''
+    },
+    value: {
+      type: String,
+      value: "",
+    },
+    type: String, // 简化的定义方式
+  },
+  methods: {
+    changeValue(e) {
+      this.triggerEvent("inputChange", e.detail);
+    },
+  },
 });
