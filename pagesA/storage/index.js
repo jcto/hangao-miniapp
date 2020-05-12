0390f699a0b588b7472cc7aca2ed6707370adef3
cao.huanan <cao.huanan@opg.cn>
2020-05-12 17:03:55 +0800

    pagesA

--- pagesA/storage/index.js
+++ pagesA/storage/index.js
@@ -1,441 +1,66 @@
-const app = getApp();
-import { loginModules } from "../../modules/login.js";
-let LoginModules = new loginModules();
-
-
-
+// pages/test/index.js
 Page({
-  /**
-   * 页面的初始数据
-   */
 
-  data: {
-    navTitle: "",
-   
-    DocQty: "暂无",
-    OutStockDate: "暂无",
-    PackageQty: "暂无",
-
-    DnNum: "",//dn号
-    StartSite:"",
-    dnTips: "",
-    Tipsflag: false, //dn号提示语显示隐藏
-    tkDn:"",
-    total:0 ,//桶数,
-    totalflag:false,
-    totalTips:"",
-   
-   
-   
-   
-   
-    addDnStartPlace:"",
-    
-    startPlaceTips: "点击选择仓库",
-    endtPlaceTips: "点击选择仓库",
-
-    showPopupFlag:false, //双向栏显示隐藏
-    CityAndStorage:[],
-    endPopupFlag:false
-   
-  },
-
-  onChange(event) {
-    const { picker, value, index } = event.detail;
-    picker.setColumnValues(1,this.data.cityStore[value[0]]);
-  },
-
-  goPrev() {
-    wx.reLaunch({
-      url: './../../pages/index/index',
-    })
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-   this.getCityAndStroage();
+    /**
+     * 页面的初始数据
+     */
+    data: {
   
-   
-  },
+    },
   
-  // todo
-
-
-  goBack() {
-    this.goPrev()
-  },
-
-
-
-  inputEnd() {
-//  && this.data.DnNum !== "000000"
-    if (this.data.Tipsflag ) {
-      this.getStockInTran();
-      wx.setStorageSync("inNumber", this.data.DnNum)
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
   
-    }  
-
-  },
-  changeDnNum(e) {
-    this.checkDN(e.detail)
-    // console.log(e.detail)
-    this.data.DnNum = e.detail
-
-  },
-  changeTotal(e){
-    console.log(e.detail,"inTotal"),
-    this.data.total = e.detail
-    this.checkTotal(e.detail)
+    },
   
-  },
-
-  totalInputEnd(){
- 
-    wx.setStorageSync("inTotal", this.data.total)
-  },
-  // 效验桶数
-  checkTotal(total){
-    // console.log(Number(total))
-    let barrelage = (parseInt(total))
-    if (isNaN(Number(total))){
-      this.setData({
-        totalTips: "数量只能为数字",
-        totalflag: false,
-        total: barrelage
-      })
-    }else if(parseInt(total) === 0){
-      this.setData({
-        totalTips: "数量不能为零",
-        totalflag: false,
-        total: barrelage
-      })
-    }
-    else if (!total){
-        this.setData({
-          totalTips: "数量不能为空",
-          totalflag: false,
-          total: barrelage
-      })
-    }else{
-      this.setData({
-        totalTips: "",
-        totalflag: true,
-        total: barrelage
-      })
-    }
-  },
-
-  // 增加起始站点和目的站点效验
-  _checkDestination() {
-    let instartID =wx.getStorageSync("instartID");
-    let inendID = wx.getStorageSync("inendID")
-    console.log(instartID, inendID,"_checkDestination")
-    if (instartID == "" || inendID == ""){
-      if (instartID == "" ){
-        wx.showToast({
-          title: '起始站点不存在',
-          icon: 'none',
-
-        })
-      }else{
-        wx.showToast({
-          title: '目的站点不存在',
-          icon: 'none',
-
-        })
-      }
-      return false
-    }
-    else if (instartID === inendID) {
-      wx.showToast({
-        title: '起始站点和目的站点不能一致',
-        icon: 'none',
-
-      })
-      return false
-    } else {
-      return true
-    }
-
-   
-  },
-
-
-
-  // 单号效验
-  checkDN(DN) {
-    // console.log(DN, "DN")
-    let arr1 = "";
-    if (DN.length >= 3) {
-      arr1 = DN.slice(0, 2);
-      arr1 = parseInt(arr1);
-    }
-
-    let DNLIst = DN.split('');
-    
-    let DNLISTNAN = false
-  DNLIst.forEach(   item=>{
-     
-      if(isNaN(Number(item))){
-        DNLISTNAN=true
-       
-      }
-   
-    })
-
-   
-
-    if (!DN) {
-      this.setData({
-        dnTips: "DN号不能为空",
-        Tipsflag: false
-
-      })
-    } else if (DNLISTNAN){
-      this.setData({
-        dnTips: "DN单号只能是数字",
-        Tipsflag: false
-
-      })
-
-    }else if (DN.length !== 10) {
-
-      this.setData({
-        dnTips: "DN单号必须为10位",
-        Tipsflag: false
-
-      })
-    } else if (arr1 !== 99 && arr1 !== 88 && arr1 !== 77 && arr1 !== 66 && arr1 !== 55 && arr1 !== 44 && arr1 !== 33 && arr1 !== 22 && arr1 !== 11) {
-
-      this.setData({
-        dnTips: "DN单号必须为99-11开头",
-        Tipsflag: false
-
-      })
-
-
-    } else {
-      this.setData({
-        dnTips: "",
-        Tipsflag: true
-
-      })
-    }
-  },
-  // 扫码
-  scan() {
-    wx.scanCode({
-      success: (res) => {
-        
-
-        this.setData({
-          DnNum: res.result
-        })
-        this.checkDN(res.result)
-       
-        if (this.data.Tipsflag){
-       
-         this.getStockInTran()
-         
-          wx.setStorageSync("inNumber", this.data.DnNum)
-       }
-      },
-      fail: (res) => {
-        // console.log(res);
-      }
-
-    })
-  },
-  getStockInTran() {
-    let dn = this.data.DnNum;
-    LoginModules.getOutDn(dn,"入库").then(res => {
-      this.setData({
-        DocQty: res.DocQty,
-        OutStockDate: res.OutStockDate,
-        PackageQty: res.PackageQty,
-        StartSite: res.StartSite,
-        DnInfoFlag:true,
-      })
-      this.getStrageID(res.StartSite, "instartID",true)
-
-    }).catch(err => {
-      
-      this.setData({
-        
-        DocQty: "暂无",
-        OutStockDate: "暂无",
-        PackageQty: "暂无",
-        StartSite: "",
-        DnInfoFlag: false,
-       
-      });
-    
-    })
-  },
-
-  next() {
-    
-    // 为空判断
-    if (!this.data.totalflag || !this.data.Tipsflag  ){
-      this.checkDN(this.data.DnNum);
-      this.checkTotal(this.data.total) 
-      return
-      
-    }
-    // 检测起始地目的地
-    let checkDestination = this._checkDestination();
-    if (!checkDestination) return
-    // 防止dn单号未缓存
-      let storagesDn = wx.getStorageSync("inNumber");
-      if (!storagesDn){
-        wx.setStorageSync("inNumber", this.data.DnNum)
-      }
-        wx.navigateTo({
-        url: '../storageList/index',
-      })
-    
-    
-  },
-
-
-
-
-
-//  请求以及格式化数据
-  getCityAndStroage() {
-    LoginModules.getCity().then(res => {
-      this.data.CityAndStorage = res
-      let city =  this.classify(res);
-      let cityStore = {}
-      let AllDataArray = []
-      let itemArray = [];
-      for (let i = 0; i < city.length; i++) { 
-        itemArray[i]=[]
-        for(let k =0;k< city[i].allData.length;k++){
-          itemArray[i].push(city[i].allData[k].SiteName)
-          console.log(city[i].allData[k].SiteName)
-        }  
-        cityStore[city[i].City] = itemArray[i]
-      }
-      let columns= [
-        {
-          values: Object.keys(cityStore),
-          className: 'column1'
-      },
-        {
-          values: cityStore['成都'],
-          className: 'column2',
-          defaultIndex: 0
-      }
-      ]
-      this.setData({
-        columns,
-        cityStore,
-        storeageIsOk: true,
-      })
-    })  
-  },
-
-  // 起始地仓库唤醒
-  showStorage(){
-    this.setData({
-      showPopupFlag:true
-    }) 
-  },
-  showendStorage(){
-    this.setData({
-      endPopupFlag: true
-    }) 
-  },
-  // 目的地确定下拉框
-  onendConfirm(event){
-    const { picker, value, index } = event.detail;
-    this.setData({
-      endPopupFlag: false,
-      endtPlaceTips: value
-    })
-
-    console.log(`当前值：${value}, 当前索引：${index}`);
-    this.getStrageID(value, "inendID")
-  },
-  onConfirm(event) {
-    const { picker, value, index } = event.detail;
-    this.setData({
-      showPopupFlag:false,
-      startPlaceTips: value
-    })
-    
-    this.getStrageID(value, "instartID")
-    
-  },
-
-  // 获取仓库id值
-  getStrageID(value, setStorageNAme, warehouseTrue=false){
-    let allStorgae = this.data.CityAndStorage;
-    if (!warehouseTrue){
-     
-      allStorgae.forEach(item => {
-        if (item.SiteName === value[1] && item.City === value[0]) {
-          wx.setStorageSync(setStorageNAme, item.SiteId)
-        }
-      })
-    }else{
-      allStorgae.forEach(item => {
-        if (item.SiteName === value) {
-          wx.setStorageSync(setStorageNAme, item.SiteId)
-        }
-      })
-    }
-    
-  },
-
-  onCancel(event) {
-    this.setData({
-      showPopupFlag: false,
-      endPopupFlag: false
-    })
-    
-  },
-
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
   
+    },
   
-  onClose(){
-    this.setData({
-      showPopupFlag: false,
-      endPopupFlag:false
-    })
-  },
- 
-  // 格式化城市数组
-  classify(arr) {
-    let c = [];
-    let d = {};
-    arr.forEach(element => {
-      if (!d[element.City]) {
-        c.push({
-          City: element.City,
-          allData: [element]
-        });
-        d[element.City] = element;
-
-      } else {
-        c.forEach(ele => {
-          if (ele.City == element.City) {
-            ele.allData.push(element);
-          }
-        });
-      }
-
-    });
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
   
-    return c;
-  },
+    },
   
-
- 
-});
-
-
-
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+  
+    },
+  
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+  
+    },
+  
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+  
+    },
+  
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+  
+    }
+  })
--- pagesA/storageList/index.js
+++ pagesA/storageList/index.js
@@ -1,162 +1,66 @@
-// pagesA/storageList/index.js
-let app = getApp();
+// pages/test/index.js
 Page({
-  data: {
-    show: true,
-    transitionModel: "",
-    list: [],
-    DN: wx.getStorageSync("inNumber"),
-   
-    total: wx.getStorageSync("inTotal") ,//总桶数
-    isShow:false,
-    TKDN:""
-  },
 
+    /**
+     * 页面的初始数据
+     */
+    data: {
   
-  // 删除
-  onClose(e) {
-    let index = e.currentTarget.dataset.index;
-   
-    if (e.target.id == "close") {
-      this.showToast({
-        msg: "是否删除"
-      }).then(res => {
-        this._removeList(index)
-        wx.setStorageSync("inTankList", this.data.list)
-      })
-    }
-  },
-  TKInputEnd(e){
-    let inputTK = e.detail.value;
-    if (inputTK){
-      this.tkCheck(inputTK)
-    }
-    
-
-  },
-
-  _removeList(index) {
-    this.data.list.splice(index, 1)
-    let tkDN = this.data.list;
-    this.setData({
-      list: tkDN
-    })
-  },
-  // 扫码
-  scan() {
-    let that = this;
-    let list = this.data.list;
-    
-    wx.scanCode({
-      success: (res) => {
-       
-        let result = res.result;
-        // if (result.length === 8) {
-        //   console.log(res.result)
-        //   list.unshift(result);
+    },
   
-        //   let newList = [...new Set(list)]
-         
-        //   that.setData({
-        //     list: newList,
-           
-          
-        //   })
-
-        // } else {
-        //   wx.showToast({
-        //     title: 'DN单号必须为八位',
-        //     icon: "none"
-        //   })
-        // }
-        that.tkCheck(result)
-      },
-      fail: (res) => {
-        console.log(res);
-        wx.showToast({
-          title: '扫码失败，请重试',
-          icon:"none"
-        })
-      }
-
-    })
-  },
-
-  // tkCheck
-  tkCheck(result){
-    let list = this.data.list;
-    if (result.length === 8) {
-     
-      list.unshift(result);
-
-      let newList = [...new Set(list)]
-
-      this.setData({
-        list: newList,
-
-
-      })
-      wx.setStorageSync("inTankList", this.data.list)
-    } else {
-      wx.showToast({
-        title: 'Tank编号为八位,请扫码Tank二维码',
-        icon: "none"
-      })
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+  
+    },
+  
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+  
+    },
+  
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+  
+    },
+  
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+  
     }
-  },
-
-  // 提示框
-  showToast(params) {
-    return new Promise((resole, reject) => {
-      this._showToast(params, resole, reject)
-    })
-  },
-  _showToast(params, resole, reject) {
-    wx.showModal({
-      title: params.title || "提示",
-      content: params.msg,
-      success(res) {
-        if (res.confirm) {
-          resole(res)
-        }
-      }
-    })
-  },
-  // 上一页
-  goBack() {
-    wx.setStorageSync("inTankList",this.data.list )
-    wx.navigateBack({})
-  },
- 
-  // 下一页
-  next() {
-
-    if (this.data.total != this.data.list.length){
-     this.setData({
-       isShow:true
-     })
-      return 
-    }
-    // 储存tk列表的值
-    wx.setStorageSync("inTankList", this.data.list)
-   
-    
-    wx.navigateTo({
-      url: '../explain/index',
-    })
-  },
-  onShow:function(){
-   let tNum = wx.getStorageSync("inTotal");
-    let list = wx.getStorageSync("inTankList");
-    if(!list){
-      list = []
-    };
-    let inNumber = wx.getStorageSync("inNumber");
-    console.log("tNum",tNum)
-   this.setData({
-     total: tNum,
-     DN: inNumber,
-     list
-   })
-  }
-})
+  })
