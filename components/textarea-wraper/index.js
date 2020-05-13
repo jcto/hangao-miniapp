46eb7ab93bb9537799d96bba48c924850ea05b04
cao.huanan <cao.huanan@opg.cn>
2020-05-13 17:24:02 +0800

    完成调试入库逻辑

---
+++ components/textarea-wraper/index.js
@@ -0,0 +1,51 @@
+Component({
+  properties: {
+    label: {
+      // 属性名
+      type: String,
+      value: "账号:",
+    },
+    isRightIcon: {
+      type: Boolean,
+      value: false,
+    },
+    titleWidth: {
+      type: String,
+      value: "100rpx",
+    },
+    required: {
+      type: Boolean,
+      value: true,
+    },
+    clearable: {
+      type: Boolean,
+      value: true,
+    },
+    placeholder: {
+      type: String,
+      value: "",
+    },
+    readonly: {
+      type: Boolean,
+      value: false,
+    },
+    value: {
+      type: String,
+      value: "",
+    },
+    type: String, // 简化的定义方式
+  },
+  data: { autoSize: { maxHeight: 200, minHeight: 50 } },
+  methods: {
+    showTips(e) {
+      this.triggerEvent("onFocus", e.detail);
+    },
+    hideTips(e) {
+      this.triggerEvent("onBlur", e.detail);
+    },
+    inputKey(e) {
+      this.triggerEvent("onInput", e.detail);
+    }
+    
+  },
+});
