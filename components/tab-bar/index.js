895462f56b95649797b000dad0b94e9d721cbb6d
zp <zp@mac.cn>
2020-06-29 23:13:50 +0800

    save

---
+++ components/tab-bar/index.js
@@ -0,0 +1,34 @@
+Component({
+    data: {
+        active: 0, 
+        show: false,
+    },
+    methods: {
+        showPopup() {
+            this.setData({ show: true });
+          },
+        
+          onClose() {
+            this.setData({ show: false });
+          },
+        onChange(event) {
+            const detail = event.detail;
+            if (detail === 1) {
+                console.log("scan");
+                this.setData({ show: true });
+                return
+            }
+            if (detail === 0) {
+                console.log("home");
+
+            }
+            if (detail === 2) {
+                console.log("person");
+
+            }
+            // event.detail 的值为当前选中项的索引
+            this.setData({ active: event.detail });
+        },
+    }
+
+})
