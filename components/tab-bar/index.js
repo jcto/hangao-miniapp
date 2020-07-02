b5cbe16d533a1a22706d6624d0b5984088ff32d9
cao.huanan <cao.huanan@opg.cn>
2020-07-02 10:26:39 +0800

    就等接口联调了

--- components/tab-bar/index.js
+++ components/tab-bar/index.js
@@ -1,34 +1,48 @@
+import { loginModules } from "../../modules/login.js";
+let LoginModules = new loginModules();
 Component({
-    data: {
-        active: 0, 
-        show: true,
+  data: {
+    active: 0,
+    show: false,
+  },
+  methods: {
+    showPopup() {
+      this.setData({ show: true });
     },
-    methods: {
-        showPopup() {
+
+    onClose() {
+      this.setData({ show: false });
+    },
+    scan() {
+      let _this = this;
+      wx.scanCode({
+        success: (res) => {
+          let result = res.result;
+          LoginModules.getOutCity().then((res) => {
             this.setData({ show: true });
-          },
-        
-          onClose() {
-            this.setData({ show: false });
-          },
-        onChange(event) {
-            const detail = event.detail;
-            if (detail === 1) {
-                console.log("scan");
-                this.setData({ show: true });
-                return
-            }
-            if (detail === 0) {
-                console.log("home");
-
-            }
-            if (detail === 2) {
-                console.log("person");
-
-            }
-            // event.detail 的值为当前选中项的索引
-            this.setData({ active: event.detail });
+          });
         },
-    }
-
-})
+        fail: (res) => {
+          console.log(res);
+        },
+      });
+    },
+    onChange(event) {
+      const detail = event.detail;
+      if (detail === 1) {
+        console.log("scan");
+        // 这里调用涉像头
+        this.scan();
+        return;
+      }
+      if (detail === 0) {
+        console.log("home");
+      }
+      if (detail === 2) {
+        console.log("person");
+      }
+      // event.detail 的值为当前选中项的索引
+      this.setData({ active: event.detail });
+    },
+  },
+});
