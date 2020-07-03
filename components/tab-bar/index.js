ed6c7ff11ead7317c432c2992569890e20af41ca
cao.huanan <cao.huanan@opg.cn>
2020-07-03 09:44:43 +0800

    调试优化

--- components/tab-bar/index.js
+++ components/tab-bar/index.js
@@ -2,9 +2,19 @@ import { loginModules } from "../../modules/login.js";
 let LoginModules = new loginModules();
 Component({
   data: {
-    active: 0,
+    active: null,
     show: false,
   },
+  properties: {
+    pageType: {
+        // 属性名
+        type: Number,
+        value: 0,
+      },
+    },
+    ready: function() { 
+        this.setData({active:this.data.pageType})
+    },
   methods: {
     showPopup() {
       this.setData({ show: true });
@@ -16,7 +26,7 @@ Component({
     scan() {
       let _this = this;
       this.setData({ show: true });
-return
+      return;
       wx.scanCode({
         success: (res) => {
           let result = res.result;
@@ -38,10 +48,19 @@ return
         return;
       }
       if (detail === 0) {
+        if (this.active === 0) return;
+        wx.redirectTo({
+          url: "/pages/index/index",
+        });
         console.log("home");
       }
       if (detail === 2) {
         console.log("person");
+        if (this.active === 2) return;
+
+        wx.redirectTo({
+          url: "/pages/user/index",
+        });
       }
       // event.detail 的值为当前选中项的索引
       this.setData({ active: event.detail });
